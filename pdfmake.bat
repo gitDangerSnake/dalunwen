8ca779ec61ff4a6bb5fe5e74f3a5740799045883
zdw_mzsf <zdw_mzsf@163.com>
2015-03-13 18:21:57 +0800

    2015/3/13

---
+++ pdfmake.bat
@@ -0,0 +1,7 @@
+set ARTICLE=hnumain
+latex -synctex=1 %ARTICLE%
+bibtex %ARTICLE%
+latex -synctex=1 %ARTICLE%
+latex -synctex=1 %ARTICLE%
+dvipdfmx  %ARTICLE%.dvi
+FoxitReader hnumain.pdf
---
+++ preview.bat
@@ -0,0 +1,1 @@
+FoxitReader hnumain.pdf
